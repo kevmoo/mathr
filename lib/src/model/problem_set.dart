58ad2b640977bc943fb5c5e0b002845e93b1cbc7
Kevin Moore <kevmoo@google.com>
2020-01-28 09:31:06 -0800

    code reorg

--- lib/src/model/problem_set.dart
+++ lib/src/model/problem_set.dart
@@ -8,46 +8,6 @@ import 'package:stats/stats.dart';
 import '../util.dart';
 import 'problem_data.dart';
 
-class _ProblemScore {
-  static const _maxEntries = 5;
-  final _entries = ListQueue<_ScoreEntry>();
-
-  void _scoreCurrentProblem(Problem problem, Duration elapsed) {
-    _entries.add(_ScoreEntry(problem.solved, problem.wrongAnswers, elapsed));
-
-    while (_entries.length > _maxEntries) {
-      _entries.removeLast();
-    }
-  }
-
-  num get average => _entries.isEmpty
-      ? null
-      : _entries.map((element) => element.points).average;
-}
-
-class _ScoreEntry {
-  static const _maxPoints = 10;
-
-  final bool solved;
-  final int incorrectAttempts;
-  final Duration elapsed;
-
-  _ScoreEntry(this.solved, this.incorrectAttempts, this.elapsed)
-      : assert(incorrectAttempts >= 0),
-        assert(!elapsed.isNegative);
-
-  bool get skipped => !solved;
-
-  int get points {
-    if (!solved) {
-      return _maxPoints;
-    }
-
-    // no more than 10 – or other value DBD
-    return math.min(incorrectAttempts * 2 + elapsed.inSeconds, _maxPoints);
-  }
-}
-
 abstract class ProblemSet<T, PD extends ProblemData<T>> extends ChangeNotifier {
   final _watch = Stopwatch()..start();
   final _problems = SplayTreeMap<PD, _ProblemScore>();
@@ -126,5 +86,45 @@ abstract class ProblemSet<T, PD extends ProblemData<T>> extends ChangeNotifier {
     if (_currentProblem.solved) {
       _newProblem();
     }
+  }
+}
+
+class _ProblemScore {
+  static const _maxEntries = 5;
+  final _entries = ListQueue<_ScoreEntry>();
+
+  void _scoreCurrentProblem(Problem problem, Duration elapsed) {
+    _entries.add(_ScoreEntry(problem.solved, problem.wrongAnswers, elapsed));
+
+    while (_entries.length > _maxEntries) {
+      _entries.removeLast();
+    }
+  }
+
+  num get average => _entries.isEmpty
+      ? null
+      : _entries.map((element) => element.points).average;
+}
+
+class _ScoreEntry {
+  static const _maxPoints = 10;
+
+  final bool solved;
+  final int incorrectAttempts;
+  final Duration elapsed;
+
+  _ScoreEntry(this.solved, this.incorrectAttempts, this.elapsed)
+      : assert(incorrectAttempts >= 0),
+        assert(!elapsed.isNegative);
+
+  bool get skipped => !solved;
+
+  int get points {
+    if (!solved) {
+      return _maxPoints;
+    }
+
+    // no more than 10 – or other value DBD
+    return math.min(incorrectAttempts * 2 + elapsed.inSeconds, _maxPoints);
   }
 }
