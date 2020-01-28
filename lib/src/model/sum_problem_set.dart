b2796197a985c06f1515371787178e4df7d15e21
Kevin Moore <kevmoo@google.com>
2020-01-27 16:26:38 -0800

    SumProblemSet cleanup

--- lib/src/model/sum_problem_set.dart
+++ lib/src/model/sum_problem_set.dart
@@ -2,35 +2,36 @@ import 'problem.dart';
 import 'problem_set.dart';
 import 'sum_problem_data.dart';
 
-Iterable<SumProblemData> _sumProblems(int lowValue, int highValue) sync* {
-  if (lowValue >= highValue) {
-    throw ArgumentError.value(
-        highValue, 'highValue', 'must be strictly greater than `lowValue`.');
-  }
-  for (var i = lowValue; i <= highValue; i++) {
-    for (var j = lowValue; j <= highValue; j++) {
-      yield SumProblemData(i, j);
-    }
-  }
-}
-
 class SumProblemSet extends ProblemSet<int, SumProblemData> {
   final int lowValue, highValue;
-
-  int get _lowAnswer => lowValue + lowValue;
-
-  /// Number of possible answers between [_lowAnswer] and [_highAnswer],
-  /// inclusive.
-  int get _range => (highValue * 2) - _lowAnswer + 1;
 
   SumProblemSet({
     this.lowValue = 0,
     this.highValue = 9,
-  }) : super(_sumProblems(lowValue, highValue));
+  })  : assert(lowValue < highValue),
+        super(_sumProblems(lowValue, highValue));
 
   @override
   Problem<int> problemFromData(SumProblemData data) => Problem(
         data,
         Iterable.generate(_range, (i) => i + _lowAnswer),
       );
+
+  /// Lowest possible value for the range provided.
+  int get _lowAnswer => lowValue + lowValue;
+
+  /// Number of possible answers given [lowValue] and [highValue].
+  int get _range => (highValue * 2) - _lowAnswer + 1;
+}
+
+Iterable<SumProblemData> _sumProblems(int lowValue, int highValue) sync* {
+  assert(
+    lowValue < highValue,
+    '`highValue` must be strictly greater than `lowValue`.',
+  );
+  for (var i = lowValue; i <= highValue; i++) {
+    for (var j = lowValue; j <= highValue; j++) {
+      yield SumProblemData(i, j);
+    }
+  }
 }
