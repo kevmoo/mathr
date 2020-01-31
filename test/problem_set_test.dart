5118531621701b976f610f8e0b76a92c8c64347a
Kevin Moore <kevmoo@google.com>
2020-01-30 18:08:23 -0800

    test cleanup

--- test/problem_set_test.dart
+++ test/problem_set_test.dart
@@ -12,31 +12,38 @@ void main() {
     expect(probSet.visitedProblemCount, 0);
   });
 
-  test('next problem should not repeat the last problem', () {
-    final probSet = SumProblemSet(lowValue: 1, highValue: 3);
+  group('next problem should', () {
+    test('not repeat, prioritize infrequently answered problems', () {
+      final probSet = _simpleProblemSet();
 
-    expect(probSet.problemCount, 9);
-    expect(probSet.visitedProblemCount, 0);
+      Problem<int> previousProblem;
+      for (var i = 0; i < 50; i++) {
+        expect(
+          probSet.currentProblem.data,
+          isNot(previousProblem?.data),
+          reason: 'Current problem should not be the same as the last problem',
+        );
+        previousProblem = probSet.currentProblem;
 
-    Problem<int> previousProblem;
-    for (var i = 0; i < 50; i++) {
-      expect(
-        probSet.currentProblem.data,
-        isNot(previousProblem?.data),
-        reason: 'Current problem should not be the same as the last problem',
-      );
-      previousProblem = probSet.currentProblem;
+        final rightAnswer = previousProblem.answers.singleWhere(
+            (element) => element.value == previousProblem.data.solution);
+        rightAnswer.onClick();
 
-      final rightAnswer = previousProblem.answers.singleWhere(
-          (element) => element.value == previousProblem.data.solution);
-      rightAnswer.onClick();
-
-      expect(
-        probSet.visitedProblemCount,
-        math.min(i + 1, probSet.problemCount),
-        reason: 'if the right answer is clicked, the visited problem count '
-            'should increase (until we hit the total problem count).',
-      );
-    }
+        expect(
+          probSet.visitedProblemCount,
+          math.min(i + 1, probSet.problemCount),
+          reason: 'if the right answer is clicked, the visited problem count '
+              'should increase (until we hit the total problem count).',
+        );
+      }
+    });
   });
+}
+
+SumProblemSet _simpleProblemSet() {
+  final probSet = SumProblemSet(lowValue: 1, highValue: 3);
+
+  expect(probSet.problemCount, 9);
+  expect(probSet.visitedProblemCount, 0);
+  return probSet;
 }
